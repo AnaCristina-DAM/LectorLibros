b922d8c2ab30380531961a52b68b1a1e2be042d4
GitHub <noreply@github.com>
2018-01-19 09:14:27 +0100

    Parte de Daniel y Azahara

--- app/src/main/java/com/example/anacristina/lectorlibros/LibroActivity.java
+++ app/src/main/java/com/example/anacristina/lectorlibros/LibroActivity.java
@@ -1,19 +1,30 @@
 package com.example.anacristina.lectorlibros;
 
+import android.content.Intent;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.Voice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.KeyListener;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class LibroActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class LibroActivity extends AppCompatActivity implements TextToSpeech.OnInitListener{
 
     private RelativeLayout l_libro;
     private TextView lb_libro;
     private EditText txt_libro;
     private ImageButton bt_reproducir;
+    String titulo;
+    String texto;
+    private TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +46,52 @@ public class LibroActivity extends AppCompatActivity {
         // BOTON - Reproducir:
         bt_reproducir = (ImageButton) findViewById(R.id.bt_reproducir);
 
+        titulo = getIntent().getStringExtra("titulo");
+        texto = getIntent().getStringExtra("texto");
+        lb_libro.setText(titulo.toString());
+        txt_libro.setText(texto.toString());
+
+        textToSpeech = new TextToSpeech( this, this );
+
+
+    }
+
+    public void escuchar(View v){
+        textToSpeech.setLanguage( new Locale( "spa", "ESP" ) );
+        speak( txt_libro.getText().toString() );
+    }
+
+    @Override
+    public void onInit( int status )
+    {
+        if ( status == TextToSpeech.LANG_MISSING_DATA | status == TextToSpeech.LANG_NOT_SUPPORTED )
+        {
+            Toast.makeText( this, "ERROR DATOS PERDIDOS | NO SOPORTADO", Toast.LENGTH_SHORT ).show();
+        }
+    }
+
+    private void speak( String str ) {
+        //textToSpeech.setVoice(new Voice());
+        textToSpeech.speak(str, TextToSpeech.QUEUE_FLUSH, null);
+        textToSpeech.setSpeechRate(0.0f);
+        textToSpeech.setPitch(0.0f);
+        //Log.v("VOZ", textToSpeech.getVoice().getName());
+        //for (Voice voice: textToSpeech.getVoices()) {
+        //Log.v("Voz",voice.getName());
+        //es-es-x-ana
+        //
+    //}
+
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        if ( textToSpeech != null )
+        {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onDestroy();
     }
 }
--- app/src/main/java/com/example/anacristina/lectorlibros/ListaActivity.java
+++ app/src/main/java/com/example/anacristina/lectorlibros/ListaActivity.java
@@ -1,5 +1,9 @@
 package com.example.anacristina.lectorlibros;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Environment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,22 +13,28 @@ import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ListaActivity extends AppCompatActivity {
 
     private RelativeLayout l_lista;
     private TextView lb_lista;
     private ListView lst_libros;
+    ArrayAdapter adaptador;
+
 
     // Creamos una lista que nos permita guardar las notas que creemos utilizando un "ArrayList":
-    ArrayList<Libro> libros = new ArrayList<Libro>();
+    ArrayList<Libro> lista_libros = new ArrayList<Libro>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
-
         // Referenciamos los distintos elementos del layout:
 
         // LAYOUT:
@@ -36,23 +46,103 @@ public class ListaActivity extends AppCompatActivity {
         // LIST VIEW:
         lst_libros = (ListView) findViewById(R.id.lst_libros);
         // Creamos un adaptador para poder visualizar las notas en la "ListView":
-        ArrayAdapter adaptador = new ArrayAdapter(this,android.R.layout.simple_list_item_1,libros);
-        lst_libros.setAdapter(adaptador);
+        adaptador = new ArrayAdapter(this,android.R.layout.simple_list_item_1,lista_libros);
+        //lst_libros.setAdapter(adaptador);
         // Ponemos los items de la lista a la escucha y establecemos los métodos "OnItemClickListener" y
         // "OnItemLongClickListener" para que actúen según los requisitos de la aplicación:
         lst_libros.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent i = new Intent (getApplicationContext(), LibroActivity.class);
+                Libro libro_selec =  lista_libros.get(position);
+                String titulo = libro_selec.getTitulo().toString();
+                String texto = libro_selec.getTexto().toString();
+                i.putExtra("titulo", titulo);
+                i.putExtra("texto", texto);
+                startActivity(i);
             }
         });
         lst_libros.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, final View view, final int position, long id) {
+                AlertDialog.Builder builder= new AlertDialog.Builder(ListaActivity.this);//Se crea el dialogo
+                builder.setMessage("¿Quiere eliminar este libro?");//Se le añade un mensaje
+                builder.setTitle("¿Estás seguro?");//Se le añade un titulo
+                /**
+                 * Se crea el boton negativo
+                 */
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        dialog.cancel();
+                    }
+                });
+                /**
+                 * Se crea el boton positivo
+                 */
+                builder.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+
+                        Libro libro_selec =  lista_libros.get(position);
+                        String titulo = libro_selec.getTitulo().toString();
+
+                        String path = Environment.getExternalStorageDirectory()+ File.separator;
+                        File f = new File(path);
+                        //obtiene nombres de archivos dentro del directorio.
+                        File file[] = f.listFiles();
+                        for (int x=0; x < file.length; x++)
+                        {
+                            if (file[x].isFile()){
+                                String nombre;
+                                nombre = file[x].getName();
+                                if (nombre.equals(titulo)){
+                                    file[x].delete();
+                                }
+                            }
+
+                        }
+                        //Configura Adapter a ListView.
+                        lista_libros.remove(position);//Se borra el elemento de la lista
+                        adaptador.notifyDataSetChanged();//Se actualiza el adaptador de la lista
+                        //lst_libros.setAdapter(adaptador);
+                    }
+                });
+                builder.show();
                 // Para evitar que se ejecute también el código del método "onClick", devolvemos un valor booleano "TRUE".
                 return true;
             }
         });
 
+        //Generacion de txt's de prueba
+        //GestionFicheros.escribirFichero("Primero.txt","Este es el primer texto");
+        //GestionFicheros.escribirFichero("Segundo.txt","Y este el segundo");
+
+        listarTXT();
+
+    }
+
+    private void listarTXT(){
+        List<String> list = new ArrayList<String>();
+        //obtiene ruta donde se encuentran los archivos.
+        String path = Environment.getExternalStorageDirectory()+ File.separator;
+        File f = new File(path);
+        //obtiene nombres de archivos dentro del directorio.
+        File file[] = f.listFiles();
+        for (int i=0; i < file.length; i++)
+        {
+            if (file[i].isFile()){
+                String nombre,texto;
+                nombre = file[i].getName();
+                texto = GestionFicheros.leerFichero(nombre);
+                Libro libro = new Libro(nombre,texto);
+               // adaptador.add(libro.getTitulo());
+                lista_libros.add(libro);
+            }
+
+        }
+        //Configura Adapter a ListView.
+        lst_libros.setAdapter(adaptador);
     }
 
 }
