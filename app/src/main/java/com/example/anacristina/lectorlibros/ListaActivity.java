dc86284acb0f1a04e14d54e15e74ad043ee0fd82
Azahara <azahara_aza@hotmail.es>
2018-01-22 18:47:11 +0100

    Prueba2

--- app/src/main/java/com/example/anacristina/lectorlibros/ListaActivity.java
+++ app/src/main/java/com/example/anacristina/lectorlibros/ListaActivity.java
@@ -43,7 +43,7 @@ public class ListaActivity extends AppCompatActivity {
         lst_libros.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                //hola
+                
             }
         });
         lst_libros.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
